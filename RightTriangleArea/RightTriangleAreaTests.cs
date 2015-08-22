57fb30526c1ecb3b6b1c24bc4ad8ee943374ce8d
Gureev Leonid <leonid.gureiev@gmail.com>
2015-08-22 14:27:46 +0300

    init

---
+++ RightTriangleArea/RightTriangleAreaTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RightTriangleArea
+{
+    [TestClass]
+    public class RightTriangleAreaTests
+    {
+        private RightTriangleHelpers _rightTriangleHelpers;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _rightTriangleHelpers = new RightTriangleHelpers();
+        }
+
+        [TestMethod]
+        public void CanCalculateRightTriangleArea()
+        {
+            const double a = 4.0;
+            const double b = 3.0;
+            const double c = 5.0;
+
+            Assert.AreEqual(6.0, _rightTriangleHelpers.CalculateArea(a, b, c),
+                _rightTriangleHelpers.Tolerance);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void CantCalculateNotRightTriangleArea()
+        {
+            const double a = 2.0;
+            const double b = 3.0;
+            const double c = 4.0;
+
+            _rightTriangleHelpers.CalculateArea(a, b, c);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void CantCalculateInvalidSideLengthTriangleArea()
+        {
+            const double a = 2.0;
+            const double b = -3.0;
+            const double c = 4.0;
+
+            _rightTriangleHelpers.CalculateArea(a, b, c);
+        }
+    }
+}
---
+++ RightTriangleArea/RightTriangleHelpers.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace RightTriangleArea
+{
+    public class RightTriangleHelpers
+    {
+        public RightTriangleHelpers(double tolerance = 1e-9)
+        {
+            Tolerance = tolerance;
+        }
+
+        public double Tolerance { get; }
+
+        private bool IsTriangleRight(double a, double b, double c)
+        {
+            return (Math.Abs(a - Math.Sqrt(b * b + c * c)) < Tolerance) ||
+                   (Math.Abs(b - Math.Sqrt(a * a + c * c)) < Tolerance) ||
+                   (Math.Abs(c - Math.Sqrt(b * b + a * a)) < Tolerance);
+        }
+
+        private void ValidatePositive(double a)
+        {
+            if (a <= 0.0)
+                throw new ArgumentOutOfRangeException(nameof(a), a,
+                    "Side of the triangle must be greater then zero.");
+        }
+
+        public double CalculateArea(double a, double b, double c)
+        {
+            ValidatePositive(a);
+            ValidatePositive(b);
+            ValidatePositive(c);
+
+            if (!IsTriangleRight(a, b, c))
+                throw new NotSupportedException("Can't calculate area of not a right triangle.");
+
+            if (a > b && a > c)
+                return (b * c) / 2.0;
+            if (b > a && b > c)
+                return (a * c) / 2.0;
+            return (a * b) / 2.0;
+        }
+    }
+}
